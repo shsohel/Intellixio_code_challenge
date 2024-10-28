1d4974d7cc35973d869c9a2383eeee8977dfe42e
SH SOHEL <49935124+shsohel@users.noreply.github.com>
2024-10-28 12:26:54 +0600

    implement model data persist even route changing

--- src/views/products/products.tsx
+++ src/views/products/products.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import { Product } from "@/types";
 import { ProductModal } from "@/views/products/productModal/productModal";
 import { BackToHome } from "@/components/backToHome/backToHome";
@@ -8,9 +8,22 @@ import { ProductList } from "@/views/products/productList/productList";
 import { PaginationControls } from "@/views/products/paginationControls/paginationControls";
 import { usePagination } from "@/hooks/usePagination";
 import { PRODUCTS_DATA } from "@/data/productsData";
+import { useRouter, useSearchParams } from "next/navigation";
 
 export const Products: React.FC = () => {
   const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const productId = searchParams.get("product-id");
+
+  // Open modal based on URL query parameter
+  useEffect(() => {
+    if (productId) {
+      const product = PRODUCTS_DATA.find((p) => p.id === productId);
+      if (product) setSelectedProduct(product);
+    }
+  }, [productId]);
+
   const {
     currentPage,
     totalPages,
@@ -18,13 +31,19 @@ export const Products: React.FC = () => {
     handlePageChange,
   } = usePagination({ items: PRODUCTS_DATA, itemsPerPage: 5 });
 
-  const handleOpenModal = useCallback((product: Product) => {
-    setSelectedProduct(product);
-  }, []);
+  const handleOpenModal = useCallback(
+    (product: Product) => {
+      router.push(`/products/?product-id=${product.id}`);
+
+      setSelectedProduct(product);
+    },
+    [router]
+  );
 
   const handleCloseModal = useCallback(() => {
     setSelectedProduct(null);
-  }, []);
+    router.push(`/products`);
+  }, [router]);
 
   return (
     <div>
@@ -36,7 +55,7 @@ export const Products: React.FC = () => {
         totalPages={totalPages}
         onPageChange={handlePageChange}
       />
-      {selectedProduct && (
+      {selectedProduct && productId && (
         <ProductModal product={selectedProduct} onClose={handleCloseModal} />
       )}
     </div>
